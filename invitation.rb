c840f452371c063f1742e939c0be8eaffe957885
AderonkeAdejare <aderonkea@comcast.net>
2018-07-25 14:13:17 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -4,12 +4,23 @@
 
 puts "What is the name of your party?"
 party_name = gets.chomp
+
 puts "What is its date?"
 date = gets.chomp
+
 puts "What time does it start?"
 time = gets.chomp
+
 puts "What is the name of the host?"
 host = gets.chomp
 
+puts "When is your RVSP date?"
+rsvp = gets.chomp
+
+puts "What is your guest's name?"
 guest_name = gets.chomp
 
+puts "Your invitation is below:"
+puts "Dear #{guest_name}, 
+
+You are cordially invited to #{party_name} on #{date} at #{time}"
